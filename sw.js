14686474dc5e5af8803d2e1219c5f58a2b3ee3be
gator2014812 <gutorov.c@yandex.ru>
2026-09-15 16:37:27 +0700

    v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Расходник — офлайн-кэш. Сеть в приоритете, кэш как запасной вариант.
-const CACHE = 'rashodnik-v2';
+const CACHE = 'rashodnik-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
